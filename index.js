e150938bab2b3c7a983f986ef0f443283fc7c95b
elisaohh <elisaohh@gmail.com>
2025-01-31 17:25:06 +0900

    add testMultifly & testDiv

--- index.js
+++ index.js
@@ -22,3 +22,11 @@ function testAdd() {
 function testSubtract() {
 	console.log(subtract(2, 1) == 1);
 }
+
+function testMultifly() {
+	console.log(multify(2, 3) == 6);
+}
+
+function testDiv() {
+	console.log(div(6, 3) == 2);
+}
